d99aea7095a30896b509a6ef8a4bf4f0bbcbaf61
Rafael Chan <rafael.chan@uol.com.br>
2024-06-22 17:36:39 -0300

    codigo

--- CNN/entrada_dados.py
+++ CNN/entrada_dados.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def monta_conjunto_dados(arquivo: str):
+def monta_conjunto_dados(arquivo: str) -> np.ndarray:
     with open(arquivo, 'r') as arq:
         dados = arq.read()
         dados_organizados = dados.replace(' ', '')
@@ -11,19 +11,19 @@ def monta_conjunto_dados(arquivo: str):
     return np.reshape(amostra_aux, (1326, 10, 12))
 
 
-def monta_rotulo(arquivo: str):
+def monta_rotulo(arquivo: str) -> np.ndarray:
     entrada_rotulo = np.loadtxt(arquivo, dtype=str)
 
     mapeamento_rotulo = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
                         'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
                         'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
 
-    rotulo = [[0 for j in range(26)] for i in range(1326)]
+    rotulo = [[0.0 for j in range(26)] for i in range(1326)]
     for i in range(0, 1326):
         for j in range(0, 26):
             if mapeamento_rotulo[entrada_rotulo[j]] == (i % 26):
-                rotulo[i][j] = 1
+                rotulo[i][j] = 1.0
             else:
-                rotulo[i][j] = 0
+                rotulo[i][j] = 0.0
 
-    return rotulo
+    return np.asarray(rotulo)
--- CNN/main.py
+++ CNN/main.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.api.models import Sequential
@@ -15,22 +14,17 @@ np.set_printoptions(threshold=np.inf)
 conjunto = entrada_dados.monta_conjunto_dados('./Arquivos/X.txt')
 rotulo = entrada_dados.monta_rotulo('./Arquivos/Y_letra.txt')
 
-conjunto_treino, resto = train_test_split(conjunto, train_size=1066, random_state=42, shuffle=False)
-
-conjunto_teste, conjunto_validacao = train_test_split(resto, test_size=0.5, random_state=42, shuffle=False)
-
-plt.imshow(conjunto_treino[0], origin='upper', vmin=-1.0, vmax=1.0)
-plt.colorbar()
-plt.show()
+conjunto_treino, conjunto_teste = train_test_split(conjunto, train_size=1066, random_state=42, shuffle=False)
+rotulo_treino, rotulo_teste = train_test_split(rotulo, train_size=1066, random_state=42, shuffle=False)
 
 model = Sequential()
 
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(10, 12, 1)))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Conv2D(60, kernel_size=3, activation='relu', input_shape=(10, 12, 1)))
+model.add(Conv2D(30, kernel_size=3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(26, activation='softmax'))
 
-for amostra in conjunto:
-    amostra_expandida = np.expand_dims(amostra, axis=2)
-    print(amostra_expandida.shape)
-    print(amostra_expandida[0])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(conjunto_treino, rotulo_treino, validation_data=(conjunto_teste, rotulo_teste), epochs=100)
+
