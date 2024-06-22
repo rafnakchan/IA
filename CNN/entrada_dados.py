4888c1abf2dbac466242d2d1a16ffad9b769389e
Rafael Chan <rafael.chan@uol.com.br>
2024-06-22 18:30:03 -0300

    codigo

--- CNN/entrada_dados.py
+++ CNN/entrada_dados.py
@@ -15,8 +15,8 @@ def monta_rotulo(arquivo: str) -> np.ndarray:
     entrada_rotulo = np.loadtxt(arquivo, dtype=str)
 
     mapeamento_rotulo = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
-                        'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
-                        'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+                         'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
+                         'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
 
     rotulo = [[0.0 for j in range(26)] for i in range(1326)]
     for i in range(0, 1326):
@@ -27,3 +27,27 @@ def monta_rotulo(arquivo: str) -> np.ndarray:
                 rotulo[i][j] = 0.0
 
     return np.asarray(rotulo)
+
+
+def verifica_resultado(resultado_obtido: np.ndarray, resultado_esperado: np.ndarray) -> (bool, str, str):
+    mapeamento_rotulo = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
+                         10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
+                         19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+
+    letra_esperada: str = ''
+    letra_predita: str = ''
+    acerto: bool = False
+
+    for i in range(0, 26):
+        if resultado_esperado[i] == 1:
+            letra_esperada = mapeamento_rotulo[i]
+        if resultado_obtido[i] > 0.9:
+            if letra_predita == '':
+                letra_predita = mapeamento_rotulo[i]
+            else:
+                letra_predita = 'Mais de uma letra predita'
+
+    if letra_esperada == letra_predita:
+        acerto = True
+
+    return acerto, letra_esperada, letra_predita
--- CNN/main.py
+++ CNN/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.api.models import Sequential
@@ -14,8 +15,11 @@ np.set_printoptions(threshold=np.inf)
 conjunto = entrada_dados.monta_conjunto_dados('./Arquivos/X.txt')
 rotulo = entrada_dados.monta_rotulo('./Arquivos/Y_letra.txt')
 
-conjunto_treino, conjunto_teste = train_test_split(conjunto, train_size=1066, random_state=42, shuffle=False)
-rotulo_treino, rotulo_teste = train_test_split(rotulo, train_size=1066, random_state=42, shuffle=False)
+conjunto_treino, conjunto_resto = train_test_split(conjunto, train_size=1066, random_state=42, shuffle=False)
+rotulo_treino, rotulo_resto = train_test_split(rotulo, train_size=1066, random_state=42, shuffle=False)
+
+conjunto_teste, conjunto_validacao = train_test_split(conjunto_resto, test_size=0.5, random_state=42, shuffle=False)
+rotulo_teste, rotulo_validacao = train_test_split(rotulo_resto, test_size=0.5, random_state=42, shuffle=False)
 
 model = Sequential()
 
@@ -26,5 +30,20 @@ model.add(Dense(26, activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(conjunto_treino, rotulo_treino, validation_data=(conjunto_teste, rotulo_teste), epochs=100)
+model.fit(conjunto_treino, rotulo_treino, validation_data=(conjunto_teste, rotulo_teste), epochs=50)
 
+acertos = 0
+for i in range(0, 130):
+    resultado = model.predict(conjunto_validacao[i:i + 1])
+    acerto, letra_esperada, letra_predita = entrada_dados.verifica_resultado(resultado[0], rotulo_validacao[i])
+    print('Acertou: ' + str(acerto))
+    print('Letra predita: ' + str(letra_predita))
+    print('Letra esperada: ' + str(letra_esperada))
+    if acerto:
+        acertos += 1
+    # plt.imshow(conjunto_validacao[i], origin='upper', vmin=0.0, vmax=1.0)
+    # plt.colorbar()
+    # plt.show()
+percentual_acertos = acertos / 130
+print('Total de acertos validação: ' + str(acertos))
+print('Percentual de acertos validação: ' + str(percentual_acertos))
