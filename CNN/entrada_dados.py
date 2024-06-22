3d35a5371be5e4805ada5b86682052b2f0f37799
Rafael Chan <rafael.chan@uol.com.br>
2024-06-21 22:23:27 -0300

    organizando pastas

--- CNN/entrada_dados.py
+++ CNN/entrada_dados.py
@@ -4,13 +4,13 @@ import numpy as np
 def monta_conjunto_dados(arquivo: str):
     with open(arquivo, 'r') as arq:
         dados = arq.read()
-        dadosOrganizados = dados.replace(' ', '').replace('-1', '0')
+        dados_organizados = dados.replace(' ', '').replace('-1', '0')
 
     rotulo = [None] * 1326
     for i in range(0, 1326):
         rotulo[i] = np.remainder(i, 26)
 
     rotulo = np.reshape(rotulo, (1326, 1))
-    amostraAux = np.fromstring(dadosOrganizados, sep=',')
+    amostraAux = np.fromstring(dados_organizados, sep=',')
 
     return np.hstack((np.reshape(amostraAux, (1326, 120)), rotulo))
