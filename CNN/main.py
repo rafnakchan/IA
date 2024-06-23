ecfd63f6bee6ea410a059d77c287d97a181634bd
Rafael Chan <rafael.chan@uol.com.br>
2024-06-22 21:14:50 -0300

    codigo

--- CNN/main.py
+++ CNN/main.py
@@ -1,20 +1,26 @@
 import numpy as np
 from keras.api.models import Sequential
 from keras.api.layers import Dense, Conv2D, Flatten
-import entrada_dados
+import dados_utilitario
 
 # importacao dos dados
 ####################################################################
 
-# conjunto de dados fornecidos pela Sarajane
-conjunto_treino, rotulo_treino, conjunto_teste, rotulo_teste, conjunto_validacao, rotulo_validacao = (
-    entrada_dados.monta_conjunto_dados('./Arquivos/X.txt', './Arquivos/Y_letra.txt'))
+dados_mnist: bool = False
+if dados_mnist:
+    # conjunto de dados mnist
+    conjunto_treino, rotulo_treino, conjunto_teste, rotulo_teste, conjunto_validacao, rotulo_validacao = (
+        dados_utilitario.monta_conjunto_dados_mnist())
+else:
+    # conjunto de dados fornecidos pela Sarajane
+    conjunto_treino, rotulo_treino, conjunto_teste, rotulo_teste, conjunto_validacao, rotulo_validacao = (
+        dados_utilitario.monta_conjunto_dados('./Arquivos/X.txt', './Arquivos/Y_letra.txt'))
 
 # configuracoes
-epocas: int = 20
+epocas: int = 5
 tamanho_amostra_validacao: int = np.size(conjunto_validacao, axis=0)
-neuronios_primeira_camada: int = 60
-neuronios_segunda_camada: int = 30
+neuronios_primeira_camada: int = 64
+neuronios_segunda_camada: int = 32
 tamanho_kernel: int = 3
 formato_imagem: tuple[int, int, int] = (np.size(conjunto_treino, axis=1), np.size(conjunto_treino, axis=2), 1)
 neuronios_camada_saida: int = np.size(rotulo_treino, axis=1)
@@ -38,7 +44,10 @@ resultado = model.predict(conjunto_validacao[:tamanho_amostra_validacao])
 acertos = 0
 for i in range(0, tamanho_amostra_validacao):
     print('****************************** ' + str(i + 1) + ' ******************************')
-    acerto, valor_esperado, valor_predito = entrada_dados.verifica_resultado(resultado[i], rotulo_validacao[i])
+    if dados_mnist:
+        acerto, valor_esperado, valor_predito = dados_utilitario.verifica_resultado_mnist(resultado[i], rotulo_validacao[i])
+    else:
+        acerto, valor_esperado, valor_predito = dados_utilitario.verifica_resultado(resultado[i], rotulo_validacao[i])
     print('Acertou: ' + str(acerto))
     print('Predito: ' + str(valor_predito))
     print('Esperado: ' + str(valor_esperado))
